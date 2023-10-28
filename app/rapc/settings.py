43965d374ab7cfb57647f34bd255ef6a1ef4d34d
cristielson-s <90371728+cristielson-s@users.noreply.github.com>
2023-10-28 13:36:45 -0300

    Update settings.py

--- app/rapc/settings.py
+++ app/rapc/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
+LogLevel=quiet
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=False)
